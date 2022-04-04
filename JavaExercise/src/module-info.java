c8f9a78496740b4fa92f6329fc4dbafbbbf4fa7f
adrienne_b <WorldofArts_95@yahoo.com>
2022-04-04 11:23:07 -0500

    first push from java to github

---
+++ JavaExercise/src/module-info.java
@@ -0,0 +1,17 @@
+module javaExercise {
+
+public class hashMap {
+	public static void main(String[] args) {
+		
+		//Illegal field name
+		int a = 1.3;
+		
+		//uninitialized field
+		 int b = 2;
+		    int c;
+		    int d = b + c;
+		    
+			System.out.println(b);
+		
+	}
+}
